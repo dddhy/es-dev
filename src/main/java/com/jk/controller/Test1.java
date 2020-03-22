07edbbe45fc0e59a845e3d8faa51ee84098fa05a
dddhy <wshongyuding@163.com>
2020-03-22 14:29:07 +0800

    Test2

--- src/main/java/com/jk/controller/Test1.java
+++ src/main/java/com/jk/controller/Test1.java
@@ -1,9 +1,31 @@
 package com.jk.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test1 {
-    public static void main(Test1[] args) {
-        java.lang.String[] arr = new java.lang.String[]{"1","2","1","2","1","3"};
-        for (int i = 0;i<arr.length;i++){
-            System.out.println(arr);
+
+    public static void main(String[] args) {
+        String[] arr = new String[]{"1","2","1","3","2","2","哈哈","哈哈"};
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i<arr.length ; i++){
+            if(map.get(arr[i])!=null){
+                map.put(arr[i],map.get(arr[i])+1);
+            }else {
+                map.put(arr[i],1);
+            }
         }
+
+        String s = "";
+        for (Map.Entry<String,Integer> entry: map.entrySet()){
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            s+= key + " ";
+            if(value>1){
+                System.out.println(key + "有"+value+"个");
+            }
+        }
+        System.out.println(s);
     }
+
 }
